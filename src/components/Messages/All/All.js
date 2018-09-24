927b2b83101778cd48c8f186f4d2700a3c346bab
Pavel Ivanov <pablospaniard@gmail.com>
2018-09-24 12:06:53 +0200

    front 1 fase done

--- src/components/Messages/All/All.js
+++ src/components/Messages/All/All.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Table } from 'reactstrap'
 
 import { NoMessages } from '../../Messages'
 
-const All = props => {
-  return <NoMessages />
+class All extends Component {
+  render() {
+    return (
+      <Table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Type</th>
+            <th>Recipient</th>
+            <th>Originator</th>
+            <th>Message</th>
+            <th>Status</th>
+            <th>Date</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>></td>
+            <td>+34672972125</td>
+            <td>Pavel</td>
+            <td>Text</td>
+            <td>delivered</td>
+            <td>09:52</td>
+          </tr>
+        </tbody>
+      </Table>
+    )
+  }
 }
 
 export default All
